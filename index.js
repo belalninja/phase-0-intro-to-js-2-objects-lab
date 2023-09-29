e5f56e810b295cb08931c32b9b25a8ab4103a558
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2023-09-29 04:43:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+  name: `Belal`,
+  streetAddress: `Ain Jalot`,
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, val) {
+  const newObj = { ...employee };
+  newObj[key] = val;
+  return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, val) {
+  obj[key] = val;
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+  const newObj = { ...obj };
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
